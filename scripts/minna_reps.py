d9f64a7cbc70ac150397ac9f52d8040e0be4d8f3
Usser65-87-11 <googa.vo1d@gmail.com>
2024-04-03 11:57:51 +0300

    up

---
+++ scripts/minna_reps.py
@@ -0,0 +1,15 @@
+import subprocess
+
+# Replace 'script.py' with the name of the script you want to run
+script_rep = '.\\scripts\\replace1_3.py'
+
+script_csv = '.\\scripts\\3_to_csv.py'
+
+# Run the script
+subprocess.run(['python', script_rep, ".\\data\\reps.txt", ".\\data\\decks\\all.csv", ".\\kotoba_csv\\out_reps.txt"])
+# subprocess.run(['python', script_rep, ".\\kotoba_csv\\out_reps.txt", ".\\data\\decks\\minna02.csv", ".\\kotoba_csv\\out_reps.txt"])
+# subprocess.run(['python', script_rep, ".\\kotoba_csv\\out_reps.txt", ".\\data\\decks\\minna03.csv", ".\\kotoba_csv\\out_reps.txt"])
+  
+
+subprocess.run(['python', script_csv, ".\\kotoba_csv\\out_reps.txt", ".\\kotoba_csv\\out_reps.csv"])
+ 
